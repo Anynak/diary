4188f7b8ba3adb955b27a6e2572fb1d7b97905de
Anynak <anynak69@gmail.com>
2022-06-29 01:39:43 +0300

    h

--- src/main/java/com/anynak/diary/config/security/SpringSecurityConfig.java
+++ src/main/java/com/anynak/diary/config/security/SpringSecurityConfig.java
@@ -1,13 +1,29 @@
-package com.anynak.diary.config;
+package com.anynak.diary.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public SpringSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return null;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().antMatchers("/","index").permitAll()
